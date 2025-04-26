d3678cc0890e99129dab61a7130d2d4008a25216
Xolmo <a20220378@pucp.edu.pe>
2025-04-25 21:59:27 -0500

    1

--- src/main/java/com/example/lab3/Repository/PacienteRepository.java
+++ src/main/java/com/example/lab3/Repository/PacienteRepository.java
@@ -2,8 +2,14 @@ package com.example.lab3.Repository;
 
 import com.example.lab3.Entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+
+    @Query(value = "SELECT * FROM pacientes p where p.doctor_id = ?1")
+    List<Paciente> obtenerPacientesPorDoctor(int doctor_id);
 }
