3af2d703330bcbf27c31716af2f3540d23371b45
Xolmo <a20220378@pucp.edu.pe>
2025-04-25 20:54:34 -0500

    navbar update!

--- src/main/java/com/example/lab3/Repository/HospitalRepository.java
+++ src/main/java/com/example/lab3/Repository/HospitalRepository.java
@@ -1,8 +1,12 @@
 package com.example.lab3.Repository;
 
+import com.example.lab3.Entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HospitalRepository extends JpaRepository<HospitalRepository, Integer> {
+public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findAll();
 }
