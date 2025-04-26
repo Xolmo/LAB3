eaf1e5e0ddb3226dcc12cb38188b5b4e663ac555
Xolmo <a20220378@pucp.edu.pe>
2025-04-25 20:48:46 -0500

    DB V2

---
+++ src/main/java/com/example/lab3/Controller/PacienteController.java
@@ -0,0 +1,7 @@
+package com.example.lab3.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PacienteController {
+}
---
+++ src/main/java/com/example/lab3/Repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.lab3.Repository;
+
+import com.example.lab3.Entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+}
