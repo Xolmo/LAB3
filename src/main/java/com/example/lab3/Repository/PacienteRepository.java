6a5b400572043645cd204c3d511a721b8b13d7c8
Xolmo <a20220378@pucp.edu.pe>
2025-05-11 23:36:01 -0500

    OK

--- src/main/java/com/example/lab3/Repository/PacienteRepository.java
+++ src/main/java/com/example/lab3/Repository/PacienteRepository.java
@@ -2,8 +2,18 @@ package com.example.lab3.Repository;
 
 import com.example.lab3.Entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.beans.Transient;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE paciente SET doctor_id = ?1, hospital_id = ?2 WHERE id = ?3", nativeQuery = true)
+    void updatePaciente(Integer doctorId, Integer hospitalId, Integer id);
 }
